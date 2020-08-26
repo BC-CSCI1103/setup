32cd0b29597bb8cfaeba72e911c276d994bb39ec
Robert Muller <robert.muller2@gmail.com>
2020-08-25 20:46:45 -0400

    added stopNgo

---
+++ stopNgo/src/bin/main.ml
@@ -0,0 +1,60 @@
+(* file: stopNGo.ml
+   author: Bob Muller
+
+   CS1103 Computer Science 1 Honors
+
+   A basic animation function stopping a ball when
+  the touchpad is released.
+*)
+
+type state = Stop | Go
+
+(* toggle : state -> state *)
+let toggle state =
+  match state with
+  | Stop -> Go
+  | Go -> Stop
+
+type model = { state : state
+             ; x : int
+             }
+
+let displayHeight = 800
+let displayWidth = displayHeight
+let delta = 2
+let radius = 100
+let circle = Image.circle radius Color.red
+let y = displayHeight / 2 - radius
+
+let backGround =
+  Image.rectangle displayWidth displayHeight Color.dodgerBlue
+
+let view model =
+  Image.placeImage circle (model.x, y) backGround
+
+let update model =
+  match model.state with
+  | Go ->
+    let x = (model.x + delta) mod displayWidth
+    in
+    { model with x = x }
+  | Stop -> model
+
+let initialModel = {state = Go; x = - radius}
+
+let handleMouse model x y event =
+  match event = "button_up" with
+  | true  -> { model with state = toggle model.state }
+  | false -> model
+
+let go () =
+  Animate.start initialModel
+    ~name: "stopNgo"
+    ~width: displayWidth
+    ~height: displayHeight
+    ~rate: 0.01
+    ~view: view
+    ~onTick: update
+    ~onMouse: handleMouse
+
+let s = go ()
---
+++ stopNgo/src/lib/assert.mli
@@ -0,0 +1,4 @@
+(* You do not need to modify this file. *)
+
+val run_test : string -> (unit -> bool) -> unit
+val run_failing_test : string -> (unit -> bool) -> unit
